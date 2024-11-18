a641b51b9eca8df29c499a6a84bc1e7df17ff059
MOHD NADEEM <72145602+Nadeem35@users.noreply.github.com>
2024-11-18 18:10:40 +0530

    Add files via upload
    Add Array Manipulation concept

---
+++ ayan/ArrayManipulation/Main.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		/*
+				int [] num = {34,5,56,43,54};
+				num[0]  = 521;
+				 num[1]  = 79;
+
+				System.out.println(Arrays.toString(num));
+		*/
+
+
+
+		/*
+
+
+				double marks[] = {78.5, 45.9, 98.8, 45.9};
+				marks[1] = 92.8;
+				System.out.println(Arrays.toString(marks));
+
+				for(int i =  0; i<marks.length; i++) {
+					System.out.println(marks[i]);
+				}
+
+		*/
+
+
+		/*
+
+				//==================== to sort array elements =================
+
+				double marks[] = {78.5, 45.9, 98.8, 45.5};
+				Arrays.sort(marks);
+
+				System.out.println(Arrays.toString(marks));
+
+				*/
+
+
+// ========== String array manipulation ==============
+
+// 		String name[] = {"Ayan", "Faizan", "Uzair", "Sheeza", "Shah", "Shafi"};
+
+
+// // 		System.out.println("Array before sort : " + Arrays.toString(name));
+
+// 		Arrays.sort(name);
+
+// 		System.out.println("Array before sort : " + Arrays.toString(name));
+
+// // 		System.out.println("The Index location of " + "'Shah' : " + Arrays.binarySearch(name, "Shah"));
+
+// 		Scanner sc = new Scanner(System.in);
+// 		String value = sc.nextLine();
+
+// 		System.out.println("The elements at Index "+ value + " is = " +  Arrays.binarySearch(name, value));
+
+
+
+
+// ===============================
+
+		try {
+			int quotient = divide(10,0);
+			System.out.println(quotient);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
