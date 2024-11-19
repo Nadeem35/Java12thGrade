5e9fc6ab05667a40e2c36529b05446539b76e705
MOHD NADEEM <72145602+Nadeem35@users.noreply.github.com>
2024-11-19 17:59:18 +0530

    Add files via upload
    Add code Exception Handling

---
+++ ayan/ExaptionHandling/Main.java
@@ -0,0 +1,105 @@
+// Java Exceptions - Try...Catch
+// When executing Java code, different errors can occur: coding errors made by the programmer, errors due to wrong input, or other unforeseeable things.
+// When an error occurs, Java will normally stop and generate an error message. The technical term for this is: Java will throw an exception (throw an error).
+
+// Java try and catch
+// The try statement allows you to define a block of code to be tested for errors while it is being executed.
+// The catch statement allows you to define a block of code to be executed, if an error occurs in the try block.
+// The try and catch keywords come in pairs:
+
+
+
+/*
+
+try{
+    //code
+}catch(Exception e){
+    System.out.print("Explain you Error");
+}
+
+*/
+
+//====================== Example - 1  =====================
+
+// public class Main{
+// 	public static void main(String[] args) {
+
+// 	    try{
+// 	    int arr[] = {1, 2, 3, 4, 5};
+// 	    System.out.print(arr[10]);
+// 	    }catch(Exception e){
+// 	        System.out.println("You are going out of bond.");
+// 	    }finally{
+// 	        System.out.println("Please correct your code!");
+// 	    }
+
+// 	}
+// }
+
+
+
+//====================== Example - 2  =====================
+
+
+// public class  Main {
+
+// 	public static int Add(int a, int b) {
+// 		return a+b;
+// // 		return a-b;
+// // 		return a*b;
+// // 		return a/b;
+// // 		return a%b;
+// 	}
+
+// 	public static void main(String[] args) {
+// 		try {
+// 			System.out.println(Add(12, 35422));
+// 		} catch(Exception e) {
+// 			System.out.println("Somthing went wrong");
+// 		}
+// 	}
+
+    
+// }
+
+
+
+//====================== Example - 3  =====================
+
+
+public class Main{
+    public static void Age(int age){
+        if(age < 18){
+            throw new ArithmeticException("You are not eligible !");
+        }else{
+            System.out.println("You are eligible Babu !");
+        }
+    }
+    
+    public static void main(String[] args){
+        Age(45);  // 
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
