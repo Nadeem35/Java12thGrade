98e5f6533dd473041530e7268daa8254333922d3
MOHD NADEEM <72145602+Nadeem35@users.noreply.github.com>
2024-12-08 22:45:34 +0530

    Update Main.java

--- ayan/GetterSetter/Main.java
+++ ayan/GetterSetter/Main.java
@@ -2,7 +2,8 @@
 /*
 
   Get and Set
- You learned from the previous chapter that private variables can only be accessed within the same class (an outside class has no access to it). However, it is possible to access them if we provide public get and set methods.
+ You learned from the previous chapter that private variables can only be accessed within the same class (an outside class has no access to it). 
+ However, it is possible to access them if we provide public get and set methods.
 
  The get method returns the variable value, and the set method sets the value.
 
