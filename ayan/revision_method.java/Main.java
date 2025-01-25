1541361adbb554b321390fbe1671ff2fe807d2bb
MOHD NADEEM <72145602+Nadeem35@users.noreply.github.com>
2025-01-25 23:58:20 +0530

    Add files via upload

---
+++ ayan/revision_method.java/Main.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Main {
+
+
+// ---------- JAVA Methods ---------
+// ---------- Task-1 ---------
+
+// 	public static void Nadeem() {
+// 		System.out.print("My first Message");
+// 	}
+
+// 	public static void main(String[] args) {
+//  	 Nadeem();
+// 	}
+
+
+// 	------------ Task-3 --------------
+// 	public static int sum(int a, int b){
+// 	   // int sum = a+b;
+// 	   // int sum = a-b;
+// 	   // int sum = a*b;
+// 	   // int sum = a/b;
+// 	    int sum = a%b;
+// 	    return sum;
+// 	}
+
+// 	public static void main(String[] age){
+// 	    int res = sum(12, 5);
+// 	    System.out.println("The sum of two number : " + res);
+// 	}
+
+
+
+// 	------------ Task-3 --------------
+	public static int area(int l, int b) {
+		int res = l*b;
+		return res;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Area of the perimeter : " + area(21, 5));
+	}
+
+
+}
+
+
+
+
+
